036fbf248dfcce7bcb7a3023ad231c04490e47d8
LK09NU22UTY <LK09NU22UTY@LK09NU22>
2018-09-29 16:20:39 +0700

    javapraktik

---
+++ java/src/java/Java.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java;
+
+/**
+ *
+ * @author LK09NU22UTY
+ */
+public class Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int i = 27;
+            int j = 56;
+            double x = 19.789;
+            double y = 8.29;
+            
+            System.out.println("variable values..");
+            System.out.println(" i = " + i);
+            System.out.println(" j = " + j);
+            System.out.println(" x = " + x);
+            System.out.println(" y = " + y);
+            
+            System.out.println("adding...");
+            System.out.println(" i + j = " + (i + j));
+            System.out.println(" x + y = " + (x + y));
+            
+            System.out.println("subtracting...");
+            System.out.println(" i - j = " +(i-j));
+            System.out.println(" x - y = " +(x-y));
+            
+            System.out.println("multipying...");
+            System.out.println("i*j="+ (i*j));
+            System.out.println("x*y+"+ (x*y));
+
+            System.out.println("dividing...");
+            System.out.println("i/j =" +(i/j));
+            System.out.println("x/j =" +(x/j));
+
+            System.out.println("computing the remaider...");
+            System.out.println("i%j =" +(i%j));
+            System.out.println("x%y =" + (x%y));
+
+            System.out.println("mixing tips...");
+            System.out.println("j+y ="+ (j+y));
+            System.out.println("i*x ="+ (i*x));// TODO code application logic here
+    }
+    
+}
